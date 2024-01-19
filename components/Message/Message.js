b1ff6528ac97d16b8719e1125ca99a70ae01a508
Ryan Griego <ryangriego@gmail.com>
2024-01-18 17:35:12 -0800

    added avatar images and changed background of message based on who is speaking

--- components/Message/Message.js
+++ components/Message/Message.js
@@ -1,7 +1,22 @@
+import { useUser } from "@auth0/nextjs-auth0/client";
+import { faRobot } from "@fortawesome/free-solid-svg-icons";
+import {  FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+
 export const Message = ({role, content}) => {
+  const { user } = useUser();
   return (
-  <div className="grid grid-cols-[30px_1fr] gap-5 p-5">
-  <div>avatar</div>
+  <div className={`grid grid-cols-[30px_1fr] gap-5 p-5 ${role === "assistant" ? "bg-gray-600" : ""
+  }`}>
+  <div>
+    {role === "user" && (
+      <img src={user.picture} width={30} height={30} alt="User avatar" className="rounded-sm shadow-md shadow-black/50"/>
+  )}
+  {role === "assistant" && (
+          <div className="flex h-[30px] w-[30px] items-center justify-center rounded-sm shadow-md shadow-black/50 bg-gray-800">
+      <FontAwesomeIcon icon={faRobot} className="text-emerald-200"/>
+    </div>
+  )}
+  </div>
     <div>{content}</div>
   </div>
   );

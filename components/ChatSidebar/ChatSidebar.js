bfa5154ec0769f9452f312bcf2e37f319483af3e
Ryan Griego <ryangriego@gmail.com>
2024-01-23 17:49:13 -0800

    added previous messages to side bar, loads chat id in url when clicked, needs styling

--- components/ChatSidebar/ChatSidebar.js
+++ components/ChatSidebar/ChatSidebar.js
@@ -1,7 +1,8 @@
 import Link from "next/link";
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 
 export const ChatSidebar = () => {
+  const [chatList, setChatList] = useState([]);
   useEffect(() => {
     const loadChatList = async () => {
       const response = await fetch(`/api/chat/getChatList`, {
@@ -9,12 +10,21 @@ export const ChatSidebar = () => {
       });
       const json = await response.json();
       console.log("CHAT LIST: ", json);
+      setChatList(json?.chats || []);
     };
     loadChatList();
   }, []);
 
   return (
-  <div className="bg-gray-900 text-white">
+  <div className="bg-gray-900 text-white flex flex-col overflow-hidden">
+      <Link href="/chat">New chat</Link>
+        <div className="flex-1 overflow-auto bg-gray-950">
+          {chatList.map(chat => (
+            <Link key={chat._id} href={`/chat/${chat._id}`}>{chat.title}</Link>
+          ))}
+        </div>
+
+
     <Link href="/api/auth/logout">
       Logout
     </Link>

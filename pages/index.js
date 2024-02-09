d4841a905caa46d9b84ae8b8010d9234c224aea2
Ryan Griego <ryangriego@gmail.com>
2024-02-09 12:36:46 -0800

    added spacing and text to the login page

--- pages/index.js
+++ pages/index.js
@@ -2,6 +2,8 @@ import Head from "next/head";
 import Link from "next/link";
 import { useUser } from "@auth0/nextjs-auth0/client";
 import { getSession } from "@auth0/nextjs-auth0";
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faRobot } from "@fortawesome/free-solid-svg-icons";
 
 export default function Home() {
 
@@ -16,13 +18,19 @@ export default function Home() {
       </Head>
       <div className="flex justify-center items-center min-h-screen w-full bg-gray-800 text-white text-center">
         <div>
-        {!!user &&<Link href="/api/auth/logout">Logout</Link>}
-        {!user && (
-        <>
-        <Link href="/api/auth/login" className="btn">Login</Link>
-              <Link href="/api/auth/signup" className="btn">Signup</Link>
-            </>
-        )}
+          <div>
+            <FontAwesomeIcon  icon={faRobot} className="text-emerald-200 text-6xl mb-2"/>
+          </div>
+          <h1 className="text-4xl font-bold">Welcome to Chat RRG</h1>
+          <p className="text-lg mt-2">Log in with your account to continue</p>
+            <div className="mt-4 flex justify-center gap-3">
+              {!user && (
+              <>
+              <Link href="/api/auth/login" className="btn">Login</Link>
+                    <Link href="/api/auth/signup" className="btn">Signup</Link>
+                  </>
+              )}
+          </div>
         </div>
       </div>
     </>
